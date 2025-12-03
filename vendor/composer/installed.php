cba529641df2dc9e45eee5e155570796caa037a2
alisher-sprouto <ali.sher@sprouto.com>
2025-12-03 16:18:07 +0500

    Clean up repository by updating .gitignore and removing tracked build/cache files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'moodle/moodle',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a331b32befc9bb013d45fbe7e66d9a78e94d5b93',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'moodle/moodle' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a331b32befc9bb013d45fbe7e66d9a78e94d5b93',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
